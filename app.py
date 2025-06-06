12807054fc6f4410c7614486b0a66a27d41743e9
furknakyol <126489856+furknakyol@users.noreply.github.com>
2025-06-06 22:21:39 +0300

    fix: db path and auto create tables

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
-
-with app.app_context():
-    db.create_all()
 class Scooter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     model = db.Column(db.String(50), nullable=False)
@@ -113,6 +111,9 @@ def maintenances():
     all_maintenances = Maintenance.query.all()
     return render_template('maintenances.html', maintenances=all_maintenances, scooters=scooters)
 
-
+with app.app_context():
+    db.create_all()
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
